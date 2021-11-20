f7319c8a43b6863cf405d52b31be83bb6a99823d
Dionathan Cordova <dionathan.cordova@gmail.com>
2021-11-20 15:14:27 -0300

    feat: e2e test user

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -3,14 +3,31 @@ import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 import { Exclude } from 'class-transformer';
 
 @Entity('c_users')
 export class Users {
+  constructor(user?: Partial<Users>) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+    this.telephone = user.telephone;
+    this.apartment = user.apartment;
+    this.password = user.password;
+    this.permission_id = user.permission_id;
+    this.avatar = user.avatar;
+    this.condominium_id = user.condominium_id;
+    this.is_admin = user.is_admin;
+    this.can_show_data = user.can_show_data;
+    this.created_at = user.created_at;
+    this.updated_at = user.updated_at;
+  }
+
   @PrimaryGeneratedColumn()
-  id: number;
+  id: string;
 
   @Column()
   name: string;
@@ -37,12 +54,15 @@ export class Users {
   @Column()
   condominium_id: number;
 
-  @CreateDateColumn({ type: 'timestamp' })
-  created_at: string;
-
   @Column()
   is_admin: boolean;
 
   @Column()
   can_show_data: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: string;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: string;
 }
---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,95 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UsersModule } from '../src/modules/users/users.module';
+import { UsersRepository } from '../src/modules/users/users.repository';
+import { v4 as uuid } from 'uuid';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+
+  const mockUser = {
+    name: 'dionathan',
+    telephone: '123',
+    apartment: '201',
+    condominium_id: 1,
+    email: 'dionathan@test.com',
+    password: uuid(),
+    permission_id: 0,
+    avatar: 'teste.png',
+    can_show_data: true,
+    is_admin: false,
+  };
+
+  const mockUsersRepository = {
+    findAll: jest.fn().mockResolvedValue(mockUser),
+    createUser: jest.fn().mockResolvedValue(mockUser),
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
+    })
+      .overrideProvider(getRepositoryToken(UsersRepository))
+      .useValue(mockUsersRepository)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+
+    await app.init();
+  });
+
+  it('/users (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/users')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(mockUser);
+  });
+
+  it('/users (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send(mockUser)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(mockUser);
+      });
+  });
+
+  it('/users (POST) -validation error', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: [
+          'name must be a string',
+          'name should not be empty',
+          'apartment must be a string',
+          'apartment should not be empty',
+          'email must be an email',
+          'email should not be empty',
+          'telephone must be a string',
+          'telephone should not be empty',
+          'password must be a string',
+          'password should not be empty',
+          'avatar must be a string',
+          'avatar should not be empty',
+          'permission_id must be a number conforming to the specified constraints',
+          'permission_id should not be empty',
+          'condominium_id must be a number conforming to the specified constraints',
+          'condominium_id should not be empty',
+          'is_admin must be a boolean value',
+          'is_admin should not be empty',
+          'can_show_data must be a boolean value',
+          'can_show_data should not be empty',
+        ],
+        error: 'Bad Request',
+      });
+  });
+});
