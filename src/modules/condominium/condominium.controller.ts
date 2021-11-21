42a6c4f4357c568092fbfe309c6ab0935da34e21
Dionathan Cordova <dionathan.cordova@gmail.com>
2021-11-20 21:48:53 -0300

    feat: user unit test service done

---
+++ src/modules/condominium/condominium.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CondominiumService } from './condominium.service';
+import { CreateCondominiumDto } from './dto/create-condominium.dto';
+import { UpdateCondominiumDto } from './dto/update-condominium.dto';
+
+@Controller('condominium')
+export class CondominiumController {
+  constructor(private readonly condominiumService: CondominiumService) {}
+
+  @Post()
+  create(@Body() createCondominiumDto: CreateCondominiumDto) {
+    return this.condominiumService.create(createCondominiumDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.condominiumService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.condominiumService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCondominiumDto: UpdateCondominiumDto) {
+    return this.condominiumService.update(+id, updateCondominiumDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.condominiumService.remove(+id);
+  }
+}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -10,22 +10,6 @@ import { Exclude } from 'class-transformer';
 
 @Entity('c_users')
 export class Users {
-  constructor(user?: Partial<Users>) {
-    this.id = user.id;
-    this.name = user.name;
-    this.email = user.email;
-    this.telephone = user.telephone;
-    this.apartment = user.apartment;
-    this.password = user.password;
-    this.permission_id = user.permission_id;
-    this.avatar = user.avatar;
-    this.condominium_id = user.condominium_id;
-    this.is_admin = user.is_admin;
-    this.can_show_data = user.can_show_data;
-    this.created_at = user.created_at;
-    this.updated_at = user.updated_at;
-  }
-
   @PrimaryGeneratedColumn()
   id: string;
 
