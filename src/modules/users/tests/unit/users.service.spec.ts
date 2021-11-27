2cebe9638b5d405dc2f37f3774237b239de3b0d7
Dionathan Cordova <dionathan.cordova@gmail.com>
2021-11-27 20:17:00 -0300

    feat: user integration test done

--- src/modules/users/tests/unit/users.service.spec.ts
+++ src/modules/users/tests/unit/users.service.spec.ts
@@ -2,8 +2,8 @@ import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { UsersRepository } from '../users.repository';
-import { UsersService } from '../users.service';
+import { UsersRepository } from '../../users.repository';
+import { UsersService } from '../../users.service';
 import { mockUser } from './mockUser';
 
 describe('UsersService', () => {
